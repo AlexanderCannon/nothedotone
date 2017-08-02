9d2faac3d408c4423ba49d91512297cb78f1f546
Alexander Cannon <a.m.t.cannon@gmail.com>
2017-08-02 14:40:03 +0100

    added tooltip function (unfinished)

--- src/components/Donut.js
+++ src/components/Donut.js
@@ -1,59 +1,93 @@
 import React, { Component } from 'react'
 import * as d3 from 'd3';
 
-function PieChart({ data, size, innerRadius }) {
-  const outerRadius = size / 2;
-
-  const colour = d3.scaleOrdinal()
-    .range([`rgba(0,0,0,${getRandomArbitrary()})`, `rgba(0,0,0,${getRandomArbitrary()})`, `rgba(0,0,0,${getRandomArbitrary()})`]);
-
-  const pie = d3
-    .pie()
-    .sort(null)
-    .value(d => d.value);
-
-  const arc = d3
-    .arc()
-    .outerRadius(outerRadius)
-    .innerRadius(innerRadius);
-
-  const labelArc = d3
-    .arc()
-    .outerRadius(outerRadius - 40)
-    .innerRadius(outerRadius - 40);
-
-  const paths = pie(data).map((d, i) => (
-    <g key={i}>
-      <path fill={colour(i)} d={arc(d)} stroke="#fff" />
-
-      <text transform={`translate(${labelArc.centroid(d)})`} dy=".35em" style={{
-        color: '#337ab7',
-        textAnchor: 'middle'
-      }}>
-        {d.data.label}
-      </text>
-    </g>
-  ));
-
-  function getRandomArbitrary(min=0, max=1) {
-    return Math.random() * (max - min) + min;
+class Donut extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      windowWidth: this.scaleWidth()
+    }
   }
 
-  return (
-    <g>
-      {paths}
-    </g>
-  );
-}
+  scaleWidth() {
+    const x = Math.max(window.innerWidth * 0.9);
+    return x > 950 ? 950 : x;
+  }
 
-class Donut extends Component {
+  handleResize(e) {
+    console.log("I ma resizied");
+    this.state = ({ windowWidth: 100 });
+    return true
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', (e) => this.handleResize);
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener('resize', (e) => this.handleResize);
+  }
+
+  PieChart({ data, size, innerRadius }) {
+    const outerRadius = size / 2;
+    const colorRange = []
+    data.forEach(() => colorRange.push(getRandomRGBAString()));
+    const colour = d3.scaleOrdinal()
+      .range(colorRange);
+
+    const pie = d3
+      .pie()
+      .sort(null)
+      .value(d => d.value);
+
+    const arc = d3
+      .arc()
+      .outerRadius(outerRadius)
+      .innerRadius(innerRadius);
+
+    const labelArc = d3
+      .arc()
+      .outerRadius(outerRadius - 2 * innerRadius)
+      .innerRadius(outerRadius - 2 * innerRadius);
+    const paths = pie(data).map((d, i) => (
+      <g key={i}>
+        <path fill={colour(i)} d={arc(d)} stroke="#fff" transform="scale(0.8)" />
+        <text transform={`translate(${labelArc.centroid(d)})`} dy=".35em" style={{
+          fill: '#222',
+          textAnchor: 'middle'
+        }}>
+          {d.data.label}
+        </text>
+        <g className="tool-tip">
+          <text x="0" y="0" font-size="25" dy="0">
+          {d.data.list.map((i) => <tspan x="0" dy="1em">{i.skill}</tspan>)}
+          </text>
+        </g>
+      </g>
+    ));
+
+    function getRandomArbitrary(min = 0, max = 1) {
+      let x = Math.random() * (max - min) + min;
+      return x > 1 ? Math.ceil(x) : x;
+    }
+
+    function getRandomRGBAString() {
+      return `rgba(${getRandomArbitrary(1, 255)},${getRandomArbitrary(1, 255)},${getRandomArbitrary(1, 255)},${getRandomArbitrary()})`;
+    }
+    return (
+      <g>
+        {paths}
+      </g>
+    );
+  }
+
   render() {
-    const { height, width, data, innerRadius } = this.props;
+    const { size, data, innerRadius } = this.props;
     return (
       <div className="chart__holder">
-        <svg width={width} height={height} className="chart__svg">
-          <g transform={`translate(${height / 2}, ${height / 2})`}>
-            <PieChart data={data} size={height} innerRadius={innerRadius} />
+        <svg width={this.state.windowWidth} height={this.state.windowWidth} className="chart__svg">
+          <g transform={`translate(${size / 2}, ${size / 2})`}>
+            <this.PieChart data={data} size={this.state.windowWidth} innerRadius={innerRadius} />
           </g>
         </svg>
       </div>
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -4,42 +4,35 @@ import { connect } from 'react-redux';
 import Donut from './Donut';
 
 class Skills extends Component {
-
-  makeChartSeries() {
-    return [
-      {
-        "field": "JavaScript",
-        "name": "JavaScript"
-      },
-      { 
-        "field": "Web",
-        "name": "Web"
-      },
-      {
-        "field": "PHP",
-        "name": "PHP"
-      }
-    ];
+  componentDidMount() {
+    window.addEventListener('resize', this.findSize)
   }
-  makeData() {
+
+  findSize() {
+    const x = Math.max(document.documentElement.clientWidth, window.innerWidth || 0) *0.9;
+    return x > 950 ? 950 : x;
+  }
+  presentData() {
     let js = this.props.skills.filter((item) => item.tag === 'JavaScript');
     let php = this.props.skills.filter((item) => item.tag === 'PHP');
     let web = this.props.skills.filter((item) => item.tag === 'Web');
+    let design = this.props.skills.filter((item) => item.tag === 'Design');
+    let sundry = this.props.skills.filter((item) => item.tag === 'Sundry');
     return [
-      { label: "JavaScript", value: js.length },
-      { label: "PHP", value: php.length },
-      { label: "Web ", value: web.length }
+      { label: "JavaScript", value: js.length, list: js },
+      { label: "PHP", value: php.length, list: php },
+      { label: "Sundry ", value: sundry.length, list: sundry},
+      { label: "Design ", value: design.length, list: design },
+      { label: "Web", value: web.length, list: web }
     ];
   }
   render() {
     return (
       <div className="skills">
         <Donut
-          width={400}
-          height={400}
-          data={this.makeData()}
-          chartSeries={this.makeChartSeries()}
-          innerRadius={75}
+          size={this.findSize()}
+          data={this.presentData()}
+          innerRadius={70}
         />
       </div>
     );
